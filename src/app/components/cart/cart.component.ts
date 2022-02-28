dae4f0a0f7a8d6e8796f5b70f9b4b058fe3ca78f
Christopher Roos <christopher.roos@iem.thm.de>
2022-02-28 17:30:46 +0100

    Cookie for Cart-service

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -13,6 +13,17 @@ export class CartComponent implements OnInit {
 
   constructor(private cartService: CartService) {}
 
+  getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
   ngOnInit() {
     this.cartService.getPizzas().subscribe((res) => {
       this.PizzasSelected = res;
--- src/app/components/choices/choices.component.ts
+++ src/app/components/choices/choices.component.ts
@@ -107,6 +107,15 @@ export class ChoicesComponent implements OnInit {
   amountOfToppings(pizza):number{
     return pizza.length-2;
   }
-  ngOnInit() {}
+  setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  }
 
+  ngOnInit() {}
 }
